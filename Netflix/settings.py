f5af3df2570e3834f95ccfc049073ed51143b9bd
Abdulaziz <programmerumar2002@.com>
2024-04-04 06:22:59 +0500

    Add STATIC_ROOT to settings

--- Netflix/settings.py
+++ Netflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b31mw%62h3wq3)0sl%#mu9o+oprxyakuh(p4fdsty4s3im9@=_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
